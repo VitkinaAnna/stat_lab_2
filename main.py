82627d78cdb947f49d1cdb0965befbe86c47c03b
VitkinaAnna <94934830+VitkinaAnna@users.noreply.github.com>
2023-12-01 05:22:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,63 +1,211 @@
 import random
 import matplotlib.pyplot as plt
-import math
 
-# Задана інтенсивність Пуассонівського потоку
-intens = 0.1  # Інтенсивність Пуассонівського потоку
-time = 500    # Кількість часових міток
-num_realizations = 1000  # Кількість реалізацій
+def generate_state_probabilities(num_states: int, add_zero: bool = False):
+    # Генерує ймовірності для кожного стану
+    probabilities = [random.randint(0, 10) for _ in range(num_states)]
+    total = sum(probabilities)
+    probabilities = [round(prob / total, 1) for prob in probabilities]
+
+    if add_zero:
+        probabilities.append(0.0)
+    random.shuffle(probabilities)
+    return probabilities
 
 
-# Ініціалізація списку значень та реалізацій
-val = [0.0 for _ in range(num_realizations)]
-realizations = [[] for _ in range(num_realizations)]
+def generate_transition_matrix(num_states: int):
+    # Генерує матрицю переходів між станами
+    transition_matrix = []
+    for _ in range(num_states):
+        transition_matrix.append(generate_state_probabilities(num_states, add_zero=True))
+    return transition_matrix
 
-# Моделювання Пуассонівського потоку
-for _ in range(time):
+
+def random_next_state(row):
+    # Вибирає наступний стан випадковим чином на основі ймовірностей у рядку
+    value = random.random()
+    state = 0
+    while value > row[state] and state < len(row) - 1:
+        value -= row[state]
+        state += 1
+    return state
+
+
+def simulate_markov_chain(initial_probabilities, transition_matrix, num_steps, num_realizations):
+    # Симулює поглинаючий ланцюг Маркова
+    num_states = len(transition_matrix)
+    states = [random_next_state(initial_probabilities) for _ in range(num_realizations)]
+    realizations = [[] for _ in range(num_realizations)]
+
+    while any(state != num_states for state in states):
+        for i in range(num_realizations):
+            realizations[i].append(states[i])
+            if states[i] != num_states:
+                states[i] = random_next_state(transition_matrix[states[i]])
+
     for i in range(num_realizations):
-        realizations[i].append(val[i])
-        val[i] += -1 / intens * math.log(1 - random.random())
+        realizations[i].append(states[i])
 
-# Візуалізація перших 6 реалізацій
-for i in range(len(realizations[:6])):
-    y = realizations[i]
-    x = [i for i in range(time)]
-    plt.plot(x, y)
-    plt.xlabel('Часовий індекс')
-    plt.ylabel('Значення')
-    plt.title(f'Реалізація {i}')
+    return realizations
+
+
+def regular_markov_chain_simulation(num_states, transition_matrix, initial_probabilities, num_steps, num_realizations):
+    # Симулює регулярний ланцюг Маркова
+    states = [random_next_state(initial_probabilities) for _ in range(num_realizations)]
+    realizations = [[] for _ in range(num_realizations)]
+
+    for _ in range(num_steps):
+        for i in range(num_realizations):
+            realizations[i].append(states[i])
+            states[i] = random_next_state(transition_matrix[states[i]])
+
+    return realizations
+
+
+def plot_markov_chain(realizations, num_states, title_prefix=''):
+    # Візуалізує ланцюг Маркова
+    for i in range(min(len(realizations), 3)):
+        realization_len = len(realizations[i])
+        end_state = realizations[i][realization_len - 1]
+        for j in range(realization_len):
+            if realizations[i][realization_len - 1 - j] != end_state:
+                end_index = j + 2
+                break
+
+        values = realizations[i][:realization_len - j + 1]
+        y = values
+        x = [i for i in range(len(values))]
+        plt.plot(x, y)
+        plt.xlabel('Крок часу')
+        plt.ylabel('Стан')
+        plt.title(f'{title_prefix} Реалізація {i}')
+        plt.show()
+
+
+def calculate_transition_matrix(realizations, num_states):
+    # Обчислює матрицю переходів на основі симуляції
+    transition_matrix = [[0.0 for _ in range(num_states + 1)] for _ in range(num_states + 1)]
+
+    for time_step in range(len(realizations[0])):
+        if time_step == 0:
+            pre_states = [row[time_step] for row in realizations]
+            continue
+        current_states = [row[time_step] for row in realizations]
+        for realization_idx in range(len(current_states)):
+            transition_matrix[pre_states[realization_idx]][current_states[realization_idx]] += 1
+        pre_states = current_states
+
+    transition_matrix = [[round(value / sum(row), 3) for value in row] for row in transition_matrix]
+    return transition_matrix
+
+
+def calculate_state_times(realizations, num_states):
+    # Обчислює середні часи перебування в кожному стані
+    num_of_steps = [0.0] * (num_states + 1)
+    duration_on_step = [0.0] * (num_states + 1)
+
+    for time_step in range(len(realizations[0])):
+        if time_step == 0:
+            pre_states = [row[time_step] for row in realizations]
+            continue
+        current_states = [row[time_step] for row in realizations]
+
+        for realization_idx in range(len(current_states)):
+            num_of_steps[current_states[realization_idx]] += 1
+
+            if current_states[realization_idx] != pre_states[realization_idx]:
+                duration_on_step[current_states[realization_idx]] += 1
+
+        pre_states = current_states
+
+    state_times = [round(num_of_steps[i] / duration_on_step[i], 3) for i in range(num_states + 1)]
+    return state_times
+
+
+def main():
+    """"""" Поглинаючий ланцюг Маркова """""""
+    num_of_states_absorbing = 5
+    transition_matrix_absorbing = [
+        [0.1, 0.2, 0.3, 0.2, 0.0, 0.2],
+        [0.2, 0.0, 0.5, 0.1, 0.1, 0.1],
+        [0.0, 0.3, 0.6, 0.1, 0.0, 0.0],
+        [0.1, 0.0, 0.1, 0.2, 0.5, 0.1],
+        [0.2, 0.0, 0.1, 0.1, 0.4, 0.2],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+    ]
+    initial_probabilities_absorbing = [0.1, 0.1, 0.2, 0.2, 0.4]
+    num_of_realizations_absorbing = 1000
+
+    realizations_absorbing = simulate_markov_chain(
+        initial_probabilities_absorbing,
+        transition_matrix_absorbing,
+        num_steps=1000,
+        num_realizations=num_of_realizations_absorbing
+    )
+
+    plot_markov_chain(realizations_absorbing, num_of_states_absorbing, title_prefix='Поглинаючий')
+
+    transition_matrix_absorbing_calculated = calculate_transition_matrix(realizations_absorbing,
+                                                                         num_of_states_absorbing)
+    for row in transition_matrix_absorbing_calculated:
+        print(row)
+
+    print("---------------")
+
+    state_times_absorbing = calculate_state_times(realizations_absorbing, num_of_states_absorbing)
+    print(state_times_absorbing)
+
+    print("---------------")
+
+    step_counts = [0.0] * len(realizations_absorbing)
+    for time_step in range(len(realizations_absorbing[0])):
+        current_states = [row[time_step] for row in realizations_absorbing]
+        for realization_idx in range(len(current_states)):
+            if current_states[realization_idx] != num_of_states_absorbing:
+                step_counts[realization_idx] += 1
+
+    print(round(sum(step_counts) / len(step_counts), 3))
+
+    print("---------------")
+
+    """""""Звичайний ланцюг Маркова"""""""""
+    num_of_states_regular = 4
+    transition_matrix_regular = [
+        [0.1, 0.1, 0.3, 0.2, 0.3],
+        [0.5, 0.1, 0.1, 0.3, 0.0],
+        [0.3, 0.1, 0.4, 0.2, 0.0],
+        [0.8, 0.1, 0.1, 0.0, 0.0],
+        [0.2, 0.5, 0.1, 0.1, 0.1],
+    ]
+    initial_probabilities_regular = [0.2, 0.2, 0.2, 0.2, 0.2]
+    num_of_realizations_regular = 1000
+    num_steps_regular = 100
+
+    realizations_regular = regular_markov_chain_simulation(
+        num_of_states_regular,
+        transition_matrix_regular,
+        initial_probabilities_regular,
+        num_steps_regular,
+        num_of_realizations_regular
+    )
+
+    plot_markov_chain(realizations_regular, num_of_states_regular, title_prefix='Регулярний')
+
+    transition_matrix_regular_calculated = calculate_transition_matrix(realizations_regular, num_of_states_regular)
+    for row in transition_matrix_regular_calculated:
+        print(row)
+
+    state_times_regular = calculate_state_times(realizations_regular, num_of_states_regular)
+    print(state_times_regular)
+
+    flat_realization = []
+    for row in realizations_regular:
+        flat_realization.extend(row)
+    plt.hist(flat_realization)
+    plt.xlabel('Стан')
+    plt.ylabel('Частота')
     plt.show()
 
-# Побудова гістограм для подій в різний момент часу
-event_indices = [1, 2, 3, 4, 5, 6]  # Вказати індекси подій для аналізу
-events = [[row[idx] for row in realizations] for idx in event_indices]
 
-for i, event in enumerate(events):
-    plt.hist(event)
-    plt.xlabel('Час появи події')
-    plt.ylabel('Кількість подій')
-    plt.title(f'Гістограма часу появи {event_indices[i]}-ої події')
-    plt.show()
-
-# Побудова гістограм для інтервалів між подіями
-intervals = [[] for i in range(num_realizations)]
-for i in range(num_realizations):
-    for j in range(time - 1):
-        intervals[i].append(realizations[i][j + 1] - realizations[i][j])
-
-for i in range(len(intervals[:6])):
-    plt.hist(intervals[i])
-    plt.xlabel('Інтервал між подіями')
-    plt.ylabel('Кількість подій')
-    plt.title(f'Гістограма інтервалів між подіями (реалізація {i})')
-    plt.show()
-
-# Побудова гістограми появи рівно n подій
-n_events = 200  # Вказати кількість подій для аналізу
-selected_events = [row[n_events - 1] for row in realizations]
-
-plt.hist(selected_events)
-plt.xlabel(f'Час появи {n_events} подій')
-plt.ylabel('Кількість подій')
-plt.title(f'Гістограма часу появи {n_events} подій')
-plt.show()
+if __name__ == "__main__":
+    main()
